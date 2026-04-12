e6ecef1f8538ed9e2bd3b79b37063691bdcafce0
Gianfranco Moran <Gianfranco Moran@DESKTOP-J3RTSR6>
2026-04-12 17:59:02 -0300

    Creo archivo ingesta de datos

---
+++ IA/00-Ingesta.R
@@ -0,0 +1,7 @@
+# Hay que instalar el paquete de google sheets con:
+# install.packages("googlesheets4")
+
+library(googlesheets4)
+url = "https://docs.google.com/spreadsheets/d/1Kwl4KByOv8q2kXMsgaO3d5QI3vUQ40RCZJgJHhg5bmE/edit?pli=1&gid=580479207#gid=580479207"
+gs4_deauth()
+datosIniciales <- read_sheet(url, sheet = 2)
