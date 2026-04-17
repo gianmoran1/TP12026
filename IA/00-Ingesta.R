c446fa36941a32142fad6af7caa9e50abd124a37
Gianfranco Moran <Gianfranco Moran@DESKTOP-J3RTSR6>
2026-04-17 19:37:19 -0300

    Manipulacion de datos

--- IA/00-Ingesta.R
+++ IA/00-Ingesta.R
@@ -1,7 +1,10 @@
 # Hay que instalar el paquete de google sheets con:
 # install.packages("googlesheets4")
 
+# Se realiza la ingesta de datos desde la hoja de datos.
+
 library(googlesheets4)
 url = "https://docs.google.com/spreadsheets/d/1Kwl4KByOv8q2kXMsgaO3d5QI3vUQ40RCZJgJHhg5bmE/edit?pli=1&gid=580479207#gid=580479207"
 gs4_deauth()
-datosIniciales <- read_sheet(url, sheet = 2, skip = 1)
+datos <- read_sheet(url, sheet = 2, skip = 1)
+str(datos)
